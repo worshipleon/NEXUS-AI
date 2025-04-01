00e0c562acedb988b35399f5f5fc49867db93a93
Pkdriller <benardwachira49@gmail.com>
2025-04-01 20:33:22 +0300

    Update GroupQuotes.js

--- commandes/GroupQuotes.js
+++ commandes/GroupQuotes.js
@@ -13,7 +13,7 @@ zokou({ nomCom: 'quote', categorie: 'Group' }, async (dest, zk, commandeOptions)
       fetch('https://animechan.xyz/api/random')
         .then((response) => response.json())
         .then(async (quote) => {
-          repondre(`*PKXMD-BOT Quotes*
+          repondre(`*QUEEN-M Quotes*
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
