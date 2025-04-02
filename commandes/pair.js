e3d1f84c389978c75d41c743153a2f2c4c0737f0
Marisel <shenapam3@gmail.com>
2025-04-02 15:31:23 +0300

    Update pair.js

--- commandes/pair.js
+++ commandes/pair.js
@@ -1,37 +1,38 @@
-const {
-  zokou
-} = require("../framework/zokou");
-const {
-  default: axios
-} = require("axios");
-zokou({
-  'nomCom': "pair",
-  'aliases': ["session", "pair", "paircode", "qrcode"],
-  'reaction': '🎀',
-  'categorie': "General"
-}, async (_0x56d90d, _0x3dcc2e, _0x1c58d3) => {
-  const {
-    repondre: _0x2c1718,
-    arg: _0x454b79
-  } = _0x1c58d3;
-  try {
-    if (!_0x454b79 || _0x454b79.length === 0) {
-      return _0x2c1718("*Add number in the Formart; 25474........*");
+const { zokou } = require("../framework/zokou");
+const { default: axios } = require("axios");
+
+zokou(
+  {
+    nomCom: "pair",
+    aliases: ["session", "pair", "paircode", "qrcode"],
+    reaction: "🎀",
+    categorie: "General",
+  },
+  async (dest, origine, msg) => {
+    const { repondre, arg } = msg;
+
+    try {
+      if (!arg || arg.length === 0) {
+        return repondre("*Please provide a number in the format: 25474........*");
+      }
+
+      await repondre("*Please wait... Generating pair code*");
+
+      const encodedNumber = encodeURIComponent(arg.join(" "));
+      const apiUrl = `https://queenmsession-5f097131c2ec.herokuapp.com/pair?number=${encodedNumber}`;
+      
+      const response = await axios.get(apiUrl);
+      const data = response.data;
+
+      if (data?.code) {
+        await repondre(data.code);
+        await repondre("*Copy the above code and use it to link your WhatsApp via linked devices*");
+      } else {
+        throw new Error("Invalid response from API - no code found");
+      }
+    } catch (error) {
+      console.error("Error getting API response:", error.message);
+      repondre("Error: Could not get response from the pairing service.");
     }
-    await _0x2c1718("*Chill......Generating*");
-    const _0x3121a0 = encodeURIComponent(_0x454b79.join(" "));
-    const _0x26074b = "https://queenmsession-5f097131c2ec.herokuapp.com/pair?number=" + _0x3121a0;
-    const _0x4b523e = await axios.get(_0x26074b);
-    const _0x3a3724 = _0x4b523e.data;
-    if (_0x3a3724 && _0x3a3724.code) {
-      const _0xc0036f = _0x3a3724.code;
-      await _0x2c1718('' + _0xc0036f);
-      await _0x2c1718("*Copy the above code and use it to link your Whatsapp via linked devices*.");
-    } else {
-      throw new Error("Invalid response from API.");
-    }
-  } catch (_0x2547cd) {
-    console.error("Error getting API response:", _0x2547cd.message);
-    _0x2c1718("Error getting response from API.");
   }
-});
+);
