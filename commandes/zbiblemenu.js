26431aa5d5fb439a6f40ba54c3d75b492718cae9
Pkdriller <benardwachira49@gmail.com>
2025-04-06 11:21:57 +0300

    Update zbiblemenu.js

--- commandes/zbiblemenu.js
+++ commandes/zbiblemenu.js
@@ -131,7 +131,7 @@ let menuMsg = `
                 externalAdReply: {
                     title: "*QUEEN-M* HOLY BIBLE LIST",
                     body: "Live with God my friends you don't know your tomorrow",
-                    thumbnailUrl: "https://files.catbox.moe/30bful.jpg",
+                    thumbnailUrl: "https://files.catbox.moe/vm9usm.jpeg",
                     sourceUrl: "https://whatsapp.com/channel/0029Vad7YNyJuyA77CtIPX0x",
                     mediaType: 1,
                     renderLargerThumbnail: true
