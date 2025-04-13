c295a43c196aed571508941fdfaf441804a608c1
Pkdriller <benardwachira49@gmail.com>
2025-04-13 13:17:37 +0300

    Update autogroup.js

--- plugins/autogroup.js
+++ plugins/autogroup.js
@@ -74,7 +74,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
         await gss.sendMessage(m.from, { text: 'An error occurred while updating the group setting.' });
       }
     }, {
-      timezone: "Asia/Kolkata"
+      timezone: "Africa/kenya"
     });
 
     m.reply(`Group will be set to "${groupSetting}" at ${time} IST.`);
