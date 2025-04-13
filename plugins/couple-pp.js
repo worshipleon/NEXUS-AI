03cce28545a9e2e6c74f5487c0e1145fcc693494
Pkdriller <benardwachira49@gmail.com>
2025-04-13 14:17:47 +0300

    Update couple-pp.js

--- plugins/couple-pp.js
+++ plugins/couple-pp.js
@@ -25,8 +25,8 @@ const couplepp = async (m, Matrix) => {
       forwardingScore: 999,
       isForwarded: true,
       forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363354023106228@newsletter',
-        newsletterName: "JawadTechX",
+        newsletterJid: '1120363288304618280@newsletter',
+        newsletterName: "pkdriller",
         serverMessageId: 143
       }
     };
