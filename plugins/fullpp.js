c64ca79fd8fa7c99ece391b1d11b9c2b3ea64f0d
Pkdriller <benardwachira49@gmail.com>
2025-04-13 14:39:22 +0300

    Update fullpp.js

--- plugins/fullpp.js
+++ plugins/fullpp.js
@@ -67,8 +67,8 @@ const setProfilePicture = async (m, sock) => {
           forwardingScore: 999,
           isForwarded: true,
           forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363398040175935@newsletter',
-            newsletterName: "JawadTechX",
+            newsletterJid: '1120363288304618280@newsletter',
+            newsletterName: "pkdriller",
             serverMessageId: 143
           }
         }
