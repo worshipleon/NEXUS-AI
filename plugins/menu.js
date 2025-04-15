1ccf3d9aec39aa3732e6a84bfc7d78f84c250058
Pkdriller <benardwachira49@gmail.com>
2025-04-16 02:06:21 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -64,7 +64,7 @@ const menu = async (m, Matrix) => {
   const mode = config.MODE === 'public' ? 'public' : 'private';
   const pref = config.PREFIX;
 
-  const validCommands = ['list', 'help', 'menu'];
+  const validCommands = ['list3', 'help', 'menu3'];
 
   if (validCommands.includes(cmd)) {
     const mainMenu = `
