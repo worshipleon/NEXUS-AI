114bcf9ad5bf9e95962dcad23350621a78ccb504
Pkdriller <benardwachira49@gmail.com>
2025-04-17 14:23:58 +0300

    Update prank-fun.js

--- plugins/prank-fun.js
+++ plugins/prank-fun.js
@@ -43,7 +43,7 @@ async (conn, mek, m, {
             '⚠️ *Note:* All actions are for demonstration purposes only.',
             '⚠️ *Reminder:* Ethical hacking is the only way to ensure security.',
             
-            '> *SHABAN-MD-HACKING-COMPLETE ☣*'
+            '> *PK-XMD-HACKING-COMPLETE ☣*'
         ];
 
         for (const line of steps) {
