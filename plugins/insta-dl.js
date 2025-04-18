d35dc0fc1258e84b3b0ed4f71aedc5b01aae3ce8
Pkdriller <benardwachira49@gmail.com>
2025-04-18 12:12:20 +0300

    Update insta-dl.js

--- plugins/insta-dl.js
+++ plugins/insta-dl.js
@@ -24,14 +24,14 @@ const instagram = async (m, Matrix) => {
     await Matrix.sendMessage(m.from, {
       video: { url: data.downloadUrl },
       mimetype: "video/mp4",
-      caption: "📥 *Powered By JawadTechX ✅*",
+      caption: "📥 *Powered By pkdriller 🇰🇪*",
       contextInfo: {
         mentionedJid: [m.sender],
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: "120363354023106228@newsletter",
-          newsletterName: "JawadTechX",
+          newsletterJid: "120363288304618280@newsletter",
+          newsletterName: "pkdriller",
           serverMessageId: 143,
         },
       },
