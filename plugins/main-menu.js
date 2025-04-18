4daad07ebae188f0e60970317cd7caeb0a3aaf8a
Pkdriller <benardwachira49@gmail.com>
2025-04-18 05:13:08 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -219,11 +219,11 @@ const menu = async (m, Matrix) => {
         menuImage = Buffer.from(response.data, 'binary');
       } catch (error) {
         console.error('Error fetching menu image from URL, falling back to local image:', error);
-        menuImage = fs.readFileSync('./media/khan.jpg');
+        menuImage = fs.readFileSync('https://i.ibb.co/bgjhrS5G/nexus-xmd.jpg');
       }
     } else {
       // Use local image if MENU_IMAGE is not configured
-      menuImage = fs.readFileSync('./media/khan.jpg');
+      menuImage = fs.readFileSync('https://i.ibb.co/bgjhrS5G/nexus-xmd.jpg');
     }
 
     await Matrix.sendMessage(m.from, {
@@ -234,8 +234,8 @@ const menu = async (m, Matrix) => {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363398040175935@newsletter',
-          newsletterName: "JawadTechX",
+          newsletterJid: '120363288304618280@newsletter',
+          newsletterName: "pkdriller",
           serverMessageId: 143
         }
       }
