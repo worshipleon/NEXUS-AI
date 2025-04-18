e96de9fc14b584975a3bfcd18eaf975b8cda7a72
Pkdriller <benardwachira49@gmail.com>
2025-04-18 12:54:52 +0300

    Update updater.js

--- plugins/updater.js
+++ plugins/updater.js
@@ -41,7 +41,7 @@ const update = async (m, Matrix) => {
 
             // Fetch latest commit hash
             const { data: commitData } = await axios.get(
-                "https://api.github.com/repos/XdTechPro/JAWAD-MD/commits/main"
+                "https://api.github.com/repos/Pkdriller/PK-XMD/commits/main"
             );
             const latestCommitHash = commitData.sha;
 
@@ -63,7 +63,7 @@ const update = async (m, Matrix) => {
             
             const response = await axios({
                 method: 'get',
-                url: "https://github.com/XdTechPro/JAWAD-MD/archive/main.zip",
+                url: "https://github.com/Pkdriller/PK-XMD/archive/main.zip",
                 responseType: 'stream'
             });
 
