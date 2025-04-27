fecbfd0dd2d7eea34a0354865c56174b8b0f2b0b
🐐 Mr pkdriller 🐐 <benardwachira49@gmail.com>
2025-04-28 02:56:51 +0300

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -35,8 +35,8 @@ async (conn, mek, m, { from, sender, reply }) => {
                 forwardingScore: 1000,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363354023106228@newsletter',
-                    newsletterName: 'JawadTechX',
+                    newsletterJid: '120363288304618280@newsletter',
+                    newsletterName: 'pkdriller',
                     serverMessageId: 143
                 }
             }
