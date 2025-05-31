65eb21f2f23a4a1349da19c030cb5215bd3d4444
⋆˚｡⋆୨✧୧˚ 𝑴𝒓 𝒘𝒊𝒔𝒆 ˚୨✧୧⋆｡˚⋆ <benardwachira49@gmail.com>
2025-05-31 10:27:40 +0300

    Add files via upload

--- pkdriller/fancy.js
+++ pkdriller/fancy.js
@@ -1,14 +1,14 @@
 const { zokou } = require("../framework/zokou");
-const fancy = require("../commandes/style");
+const fancy = require("../charles/style");
 
-zokou({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk, commandeOptions) => {
     const { arg, repondre, prefixe } = commandeOptions;
     const id = arg[0]?.match(/\d+/)?.join('');
     const text = arg.slice(1).join(" ");
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy I.T_KANJU\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('I.T_KANJU', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 TOXIC-LOVER-MD\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('TOXIC-LOVER-MD', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
