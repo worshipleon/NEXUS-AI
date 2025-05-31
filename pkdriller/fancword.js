6d90f5f602e6c2bd54023e1dbad09b59db61bb23
⋆˚｡⋆୨✧୧˚ 𝑴𝒓 𝒘𝒊𝒔𝒆 ˚୨✧୧⋆｡˚⋆ <benardwachira49@gmail.com>
2025-05-31 11:41:36 +0300

    Update fancword.js

--- pkdriller/fancword.js
+++ pkdriller/fancword.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "✍️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 CHARLESKE-XMD 2025\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('Redmi XMD 2025', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 NEXUS-AI 2025\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('NEXUS AI 2025', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
