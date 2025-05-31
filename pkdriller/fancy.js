a651548c2f773ceb8a5d05374cf46c7049c424e0
⋆˚｡⋆୨✧୧˚ 𝑴𝒓 𝒘𝒊𝒔𝒆 ˚୨✧୧⋆｡˚⋆ <benardwachira49@gmail.com>
2025-05-31 11:40:02 +0300

    Update fancy.js

--- pkdriller/fancy.js
+++ pkdriller/fancy.js
@@ -1,5 +1,5 @@
 const { zokou } = require("../framework/zokou");
-const fancy = require("../charles/style");
+const fancy = require("../nexus/style");
 
 zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk, commandeOptions) => {
     const { arg, repondre, prefixe } = commandeOptions;
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 CHARLES-XMD\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('CHARLES-XMD', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 NEXUS-AI\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('NEXUS-AI', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
