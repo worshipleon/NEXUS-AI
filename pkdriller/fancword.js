cb08076113cddd4d7ca9a7b0f71e8fa4371c45cb
⋆˚｡⋆୨✧୧˚ 𝑴𝒓 𝒘𝒊𝒔𝒆 ˚୨✧୧⋆｡˚⋆ <benardwachira49@gmail.com>
2025-05-31 11:30:23 +0300

    Add files via upload

--- pkdriller/fancword.js
+++ pkdriller/fancword.js
@@ -1,14 +1,14 @@
 const { zokou } = require("../framework/zokou");
-const fancy = require("../charles/style");
+const fancy = require("../commandes/style");
 
-zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "fancy", categorie: "Fun", reaction: "✍️" }, async (dest, zk, commandeOptions) => {
     const { arg, repondre, prefixe } = commandeOptions;
     const id = arg[0]?.match(/\d+/)?.join('');
     const text = arg.slice(1).join(" ");
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 CHARLES-XMD\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('CHARLES-XMD', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 CHARLESKE-XMD 2025\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('Redmi XMD 2025', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
