0cf3ff89ede41ced1d2df4c8cc7b32132e5d1d47
⋆˚｡⋆୨✧୧˚ 𝑴𝒓 𝒘𝒊𝒔𝒆 ˚୨✧୧⋆｡˚⋆ <benardwachira49@gmail.com>
2025-06-15 16:18:51 +0300

    Update fancy.js

--- pkdriller/fancy.js
+++ pkdriller/fancy.js
@@ -1,5 +1,5 @@
 const { zokou } = require("../framework/zokou");
-const fancy = require("../charles/style");
+const fancy = require("../pkdriller/style");
 
 zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk, commandeOptions) => {
     const { arg, repondre, prefixe } = commandeOptions;
